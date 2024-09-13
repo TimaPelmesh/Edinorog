65aecca731f48256264e3954832b58bb8f5292d1
TimaPelmesh <mr.tim.pumpkin@gmail.com>
2024-09-13 18:40:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+    from flask import Flask, render_template, request, jsonify
+    from transformers import pipeline
 
-app = Flask(__name__)
+    app = Flask(__name__)
+    generator = pipeline('text-generation', model='gpt2')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-@app.route('/data', methods=['POST'])
-def data():
-    received_data = request.json
-    # Здесь вы можете обработать полученные данные,
-    # например, выполнить некоторые вычисления или сохранить их
-    response_data = {'message': 'Данные получены', 'received': received_data}
-    return jsonify(response_data)
+    @app.route('/generate', methods=['POST'])
+    def generate():
+        user_input = request.json['input']
+        response = generator(user_input, max_length=100, num_return_sequences=1)
+        return jsonify({'generated_text': response[0]['generated_text']})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == '__main__':
+        app.run(debug=True)
+    
